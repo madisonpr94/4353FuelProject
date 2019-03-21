9afd537989cf5f6a6b7f0850e1d99c695485ee05
Acecoder <vokevin91@gmail.com>
2019-03-21 12:14:52 -0500

    Fuel Project
    Added Fuel Project Env

---
+++ env/FuelProject/FuelProjectDev/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FuelprojectdevConfig(AppConfig):
+    name = 'FuelProjectDev'
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kevinvo/School Programming/COSC 4353 Software Design/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
