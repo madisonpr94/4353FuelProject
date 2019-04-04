45430451dcb1107709d6dfa42b674202bcd12c68
Madison Pratt <madisonpr94@gmail.com>
2019-04-04 14:03:04 -0500

    removed python installation

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kevinvo/School Programming/COSC 4353 Software Design/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
